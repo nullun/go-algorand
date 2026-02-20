5b465234f582af6fcf3602089796db5a42b7a1d7
nullun <git@nullun.com>
2026-02-20 17:35:28 +0000

    test: update remaining tests to use SignatureFields struct
    Complete the SignatureFields refactoring by updating test files that
    directly construct SignedTxn structs to use the new SignatureFields
    embedded struct for signature-related fields (Sig, Lsig, AuthAddr, Msig).

--- node/node_test.go
+++ node/node_test.go
@@ -1323,7 +1323,9 @@ func TestNodeHybridP2PGossipSend(t *testing.T) {
 	}
 	signature := secrets2.Sign(txn)
 	stxn := transactions.SignedTxn{
-		Sig: signature,
+		SignatureFields: transactions.SignatureFields{
+			Sig: signature,
+		},
 		Txn: txn,
 	}
 
